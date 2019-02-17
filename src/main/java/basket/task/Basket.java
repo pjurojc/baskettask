c732e6f9d4f27f84fedcf2467f871406586c15a1
Paweł Jurojć <Pawe Juroj@pjurojc.mshome.net>
2019-02-17 18:20:40 +0100

    first test

---
+++ src/main/java/basket/task/Basket.java
@@ -0,0 +1,18 @@
+package basket.task;
+
+import baskettask.BigDecimal;
+
+public class Basket {
+
+	public void putItemsIntoBasket(String itemType, int itemsCount) {
+		for (int i=0;i<itemsCount;i++) {
+			
+		}
+	}
+
+	public BigDecimal getPriceAllItems() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/test/java/baskettask/BasketTest.java
@@ -0,0 +1,19 @@
+package baskettask;
+
+import java.math.BigDecimal;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import basket.task.Basket;
+
+public class BasketTest {
+	@Test
+	public void TestItemTypeA() {
+		Basket basket = new Basket();
+		basket.putItemsIntoBasket("A", 3);
+		BigDecimal basketPrice=basket.getPriceAllItems();
+		Assert.assertEquals(basketPrice, new BigDecimal(120L));
+	}
+
+}
